c377e94e45205846fabb12d7a230350c545d9cfa
BeMachado23 <bemachado.floripa@gmail.com>
2023-09-25 12:08:08 -0300

    primeira flatlist

--- App.tsx
+++ App.tsx
@@ -1,14 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
+
+const nomes= ["Ana Maria", "Bruno  Alves", "Carlos José", "Daniel Martins", "Efraim Gomes", "Francisco Junior"]
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+      <Text style={{fontSize:22, marginTop:100, fontWeight: 'bold'}}>Lista</Text>
+      <FlatList
+      data={nomes}
+      renderItem={({ item }) => (
+        <Text style={styles.nome}>{item}</Text>
+      )}
+      keyExtractor={(item, index) => index.toString()}
+    />
+    
+  </View>
+);
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +27,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  nome:{
+    fontSize: 18,
+    marginTop: 12,
+    borderWidth: 2,
+    textAlign: 'center'
+  }
 });
